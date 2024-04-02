a5cb55c5161779675a5d688fcba2ba092eb87f63
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-04-01 21:32:53 -0300

    fix: remove link prefetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map((product) => {
         return (
-          <Product className="keen-slider__slide" key={product.id} href={`/product/${product.id}`}>
+          <Product className="keen-slider__slide" key={product.id} href={`/product/${product.id}`} prefetch={false}>
             <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
 
             <footer>
