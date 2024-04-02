68990a84be6bc873f914d7107daea2a59d5f9fbe
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-04-01 21:32:41 -0300

    chore: remove deprecated props

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "files.stripe.com"
+    remotePatterns: [
+      {
+        hostname: 'files.stripe.com',
+      }
     ]
   }
 };
