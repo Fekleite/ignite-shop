f43cfc4b8c3759b88d8cc606f7de28ca2804bed9
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-04-10 23:43:03 -0300

    feat: add title for SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { stripe } from '@/lib/stripe';
 
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Link from 'next/link';
+import Head from 'next/head';
 
 interface HomeProps {
   products: {
@@ -28,20 +29,25 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map((product) => {
-        return (
-          <Product className="keen-slider__slide" key={product.id} href={`/product/${product.id}`} prefetch={false}>
-            <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map((product) => {
+          return (
+            <Product className="keen-slider__slide" key={product.id} href={`/product/${product.id}`} prefetch={false}>
+              <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      })}
-    </HomeContainer>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
